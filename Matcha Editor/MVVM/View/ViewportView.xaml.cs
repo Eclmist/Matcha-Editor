46d1b2da8e535a21d4b3e0678b7c9867da4f4d12
Samuel Huang <samuel.huanghaoyi@gmail.com>
2022-02-01 20:08:18 +0800

    Add draw mode dropdown

--- Matcha Editor/MVVM/View/ViewportView.xaml.cs
+++ Matcha Editor/MVVM/View/ViewportView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Matcha_Editor.Core;
+using Matcha_Editor.Core.IPC;
+using Matcha_Editor.Core.IPC.Command;
+using System.Windows.Controls;
 
 namespace Matcha_Editor.MVVM.View
 {
@@ -8,6 +11,12 @@ namespace Matcha_Editor.MVVM.View
         {
             InitializeComponent();
             EditorWindowManager.Instance.RegisterWindow(this);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            //TODO: MVVM This
+            IPCManager.Instance.Post(new SetDrawModeCommand(((ComboBoxItem)e.AddedItems[0]).Content as string));
         }
     }
 }
